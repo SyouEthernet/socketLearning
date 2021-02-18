702bf1682d4b212721cbe645bb79d8c41c231222
SyouEthernet <syouethernet@gmail.com>
2021-02-18 17:49:18 +0800

    socket demo

---
+++ src/main/java/Client.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket();
+
+        // 超时时间
+        socket.setSoTimeout(3000);
+
+        // 连接本地端口2000， 超时时间3000
+        socket.connect(new InetSocketAddress(InetAddress.getLocalHost(), 2000), 3000);
+
+        System.out.println("发起服务器连接，并且进入后续流程");
+        System.out.println("客户端信息：" + socket.getLocalAddress() + " port: "+ socket.getLocalPort());
+        System.out.println("服务器信息：" + socket.getInetAddress() + "port:" + socket.getPort());
+
+        try {
+            // send data
+            todo(socket);
+        } catch (Exception e) {
+            System.out.println("exception closed.");
+        }
+
+        socket.close();
+        System.out.println("client exit");
+    }
+
+    private static void todo(Socket client) throws IOException {
+        // construct input stream
+        InputStream in = System.in;
+        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+        // get socket output stream, and trans to printStream
+        OutputStream outputStream = client.getOutputStream();
+        PrintStream socketPrintStream = new PrintStream(outputStream);
+
+        // get socket input stream
+        InputStream inputStream = client.getInputStream();
+        BufferedReader socketBufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        boolean flag  = true;
+
+        do {
+            // read keyboard
+            String str = input.readLine();
+            // send to server
+            socketPrintStream.println(str);
+
+            // read from server
+            String echo = socketBufferedReader.readLine();
+            if ("bye".equalsIgnoreCase(echo)) {
+                flag = false;
+            } else {
+                System.out.println(echo);
+            }
+        } while(flag);
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(2000);
+
+        System.out.println("server ready");
+        System.out.println("服务器信息：" + serverSocket.getInetAddress() + " port:" + serverSocket.getLocalPort());
+
+        for (;;) {
+            // wait client connect
+            Socket client = serverSocket.accept();
+
+            // construct client thread
+            ClientHandler clientHandler = new ClientHandler(client);
+            // start thread
+            clientHandler.start();
+        }
+    }
+
+
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+        private boolean flag = true;
+
+        ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            System.out.println("new client info:" + socket.getInetAddress() + " port:" + socket.getPort());
+
+            try {
+                // get printStream, for data output
+                PrintStream socketOutput = new PrintStream(socket.getOutputStream());
+                // get input stream, for data receive
+                BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                do {
+                    //get data
+                    String str = socketInput.readLine();
+                    if ("bye".equalsIgnoreCase(str)) {
+                        flag = false;
+                        // send bye
+                        socketOutput.println("bye");
+                    } else {
+                        System.out.println(str);
+                        socketOutput.println("send:" + str.length());
+                    }
+                } while(flag);
+                socketInput.close();
+                socketOutput.close();
+
+            } catch (Exception e) {
+                System.out.println("connect exit");
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("client closed: " + socket.getInetAddress() + " port:" + socket.getPort());
+        }
+    }
+}
